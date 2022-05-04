cc6eaed2715acaba83fc6a6da5ee56cc08595491
MH <mrhh94@live.com>
2022-05-04 02:21:46 -0400

    Roboshop Project

--- components/common.sh
+++ components/common.sh
@@ -3,7 +3,7 @@ checkRootUser() {
 USER_ID=$(id -u)
 
 if [ "$USER_ID" -ne 0 ]; then
-  echo You are suppose to be running as suddo or root user
+  echo -e "\e[35mYou are suppose to be running as suddo or root user\e[0m"
   exit
 else
   echo It is ok!
