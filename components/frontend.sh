68712cc6e279f7a2d44a05c74bb87e58f6e5c8f8
MH <mrhh94@live.com>
2022-05-04 23:05:52 -0500

    Roboshop Project

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 source components/common.sh
-checkRootUser
+checkRootUser           >/tmp/roboshop.log
 
- yum install nginx -y
- systemctl enable nginx
- systemctl start nginx
+ yum install nginx -y                 >/tmp/roboshop.log
+ systemctl enable nginx                >/tmp/roboshop.log
+ systemctl start nginx                 >/tmp/roboshop.log
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"    >/tmp/roboshop.log
- cd /usr/share/nginx/html
- rm -rf *
- unzip /tmp/frontend.zip
- mv frontend-main/* .
- mv static/* .
- rm -rf frontend-main README.md
- mv localhost.conf /etc/nginx/default.d/roboshop.conf
- systemctl restart nginx
+ cd /usr/share/nginx/html         >/tmp/roboshop.log
+ rm -rf *                               >/tmp/roboshop.log
+ unzip /tmp/frontend.zip                 >/tmp/roboshop.log
+ mv frontend-main/* .                      >/tmp/roboshop.log
+ mv static/* .                            >/tmp/roboshop.log
+ rm -rf frontend-main README.md             >/tmp/roboshop.log
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf      >/tmp/roboshop.log
+ systemctl restart nginx                     >/tmp/roboshop.log
 
 
  
