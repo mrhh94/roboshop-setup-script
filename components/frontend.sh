a28e377a76debbff5800da7f16cb77d01748aad7
MH <mrhh94@live.com>
2022-05-04 23:41:43 -0500

    Roboshop Project

--- components/frontend.sh
+++ components/frontend.sh
@@ -5,33 +5,33 @@ checkRootUser           >/tmp/roboshop.log
 
  echo "Installing nginx"
  yum install nginx -y             >/tmp/roboshop.log
- checkStatus $1
+ checkStatus $?
 
  echo "Downloading frontend code files"
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"    >/tmp/roboshop.log
- checkStatus $1
+ checkStatus $?
 
  cd /usr/share/nginx/html         >/tmp/roboshop.log
 
  echo "removing old files"
  rm -rf *                         >/tmp/roboshop.log
- checkStatus $1
+ checkStatus $?
 
  echo "unzipping frontend file"
  unzip /tmp/frontend.zip          >/tmp/roboshop.log
- checkStatus $1
+ checkStatus $?
 
  echo "moving extracted files to other folder"
  mv frontend-main/* .             >/tmp/roboshop.log
  mv static/* .                    >/tmp/roboshop.log
  rm -rf frontend-main README.md   >/tmp/roboshop.log
  mv localhost.conf /etc/nginx/default.d/roboshop.conf    >/tmp/roboshop.log
- checkStatus $1
+ checkStatus $?
 
  echo "enabling nginx"
  systemctl enable nginx           >/tmp/roboshop.log
  systemctl start nginx            >/tmp/roboshop.log
  systemctl restart nginx          >/tmp/roboshop.log
- checkStatus $1
+ checkStatus $?
 
  
