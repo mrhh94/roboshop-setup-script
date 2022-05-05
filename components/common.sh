942fc3de3f45e5bef3767f6d1290fa7eef535a1f
MH <mrhh94@live.com>
2022-05-05 01:01:22 -0500

    Roboshop Project

--- components/common.sh
+++ components/common.sh
@@ -15,6 +15,7 @@ checkStatus() {
   echo -e "\e[35mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILURE\e[0m"
+  echo "please check the ${LOG_FILE}"
   exit 1
 fi
 
--- components/frontend.sh
+++ components/frontend.sh
@@ -8,7 +8,7 @@ checkRootUser                     &>>${LOG_FILE}
  checkStatus $?
 
  ECHO "Downloading frontend code files"
- cur -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"    >>${LOG_FILE}
+ cur -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"    &>>${LOG_FILE}
  checkStatus $?
 
  cd /usr/share/nginx/html         &>>${LOG_FILE}
