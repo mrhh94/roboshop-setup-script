9e259d3c773d336bffef978c37fa2e6c338b438f
MH <mrhh94@live.com>
2022-05-06 18:41:58 -0500

    Roboshop Project

--- components/create-instance.sh
+++ components/create-instance.sh
@@ -3,7 +3,7 @@
 aws ec2 run-instances \
     --image-id ami-0bb6af715826253bf \
     --instance-type t2.micro \
-    --count 1 \
+    --count 5 \
     --subnet-id subnet-550b0a18 \
     --security-group-ids sg-0cfbae2747cbac51a \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=production}]'
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test 911}]'
