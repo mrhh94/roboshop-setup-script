3d989b33f5afcb9c2714c27cb5826bc63f60887f
MH <mrhh94@live.com>
2022-05-07 00:22:04 -0500

    Roboshop Project

--- components/create-instance.sh
+++ components/create-instance.sh
@@ -1,9 +1,16 @@
+#!/usr/bin/env bash
 #aws ec2 run-instances --image-id ami-0bb6af715826253bf --instance-type t2.micro
+#aws ec2 describe-images --filters "Name=Name:Type,Values=Centos-7-DevOps-Practice" --output text
+
+#aws ec2 describe-images \
+   # --filters "Name=Name:Type,Values=Centos-7-DevOps-Practice" \
+   # --output text
 
 aws ec2 run-instances \
     --image-id ami-0bb6af715826253bf \
     --instance-type t2.micro \
-    --count 5 \
+    --count 1 \
     --subnet-id subnet-550b0a18 \
     --security-group-ids sg-0cfbae2747cbac51a \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test 911}]'
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=}]'
+
