94da17b89f8ae592026f7bb092a02eafda755545
MH <mrhh94@live.com>
2022-05-07 16:56:41 -0500

    Roboshop Project

--- components/create-spot-ec2.sh
+++ components/create-spot-ec2.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-aws ec2 run-instances --image-id ami-0bb6af715826253bf --instance-type t2.micro --count 1 --subnet-id subnet-550b0a18 --security-group-ids sg-0cfbae2747cbac51a --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value="SpotTest"}]" "ResourceType=instance,Tags=[{Key=Name,Value="SpotTest"}]"
+NAME=$1
+
+aws ec2 run-instances \
+      --image-id ami-0bb6af715826253bf \
+      --instance-type t2.micro \
+      --count 1 --subnet-id subnet-550b0a18 \
+      --security-group-ids sg-0cfbae2747cbac51a \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+      --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]"
