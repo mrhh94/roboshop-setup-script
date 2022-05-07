4a86a731fd90adc95e50ad51ba6da1819d9f7263
MH <mrhh94@live.com>
2022-05-07 16:20:09 -0500

    Roboshop Project

---
+++ components/create-spot-ec2.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+AMI_ID=$(
+  aws ec2 describe-images \
+      --filters "Name=name,Values=Centos-7-DevOps-Practice" \
+      --output table | grep ImageId | awk '{print $4}'
+)
+
+aws ec2 run-instances \
+    --image-id "${AMI_ID}" \
+    --instance-type t2.micro \
+    --count 1 \
+    --subnet-id subnet-550b0a18 \
+    --security-group-ids sg-0cfbae2747cbac51a \
+    --instance-market-options
+    MarketType: "spot", SpotOptions:{SpotInstanceType="persistent",InstanceInterruptionBehavior="stop"}
+
+
+   #--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]"
+
+    #MarketType=string,SpotOptions={MaxPrice=string,SpotInstanceType=string,BlockDurationMinutes=integer,ValidUntil=timestamp,InstanceInterruptionBehavior=string}
+
+   # {
+   #   "MarketType": "spot",
+   #   "SpotOptions": {
+   #     "MaxPrice": "string",
+   #     "SpotInstanceType": "one-time"|"persistent",
+   #     "BlockDurationMinutes": integer,
+   #     "ValidUntil": timestamp,
+   #     "InstanceInterruptionBehavior": "hibernate"|"stop"|"terminate"
+   #   }
+   # }
