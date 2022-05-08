e4084a0f99a72b1c345087cabde9077f40fe7148
MH <mrhh94@live.com>
2022-05-08 09:19:21 -0500

    Roboshop Project

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -8,23 +8,19 @@ ECHO "Installing MongoDB"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo    &>>${LOG_FILE}
 checkRootUser $?                   &>>${LOG_FILE}
 
-yum install -y mongodb-org          &>>${LOG_FILE}
-systemctl enable mongod             &>>${LOG_FILE}
+yum install -y mongodb-org  &>>${LOG_FILE} && systemctl enable mongod  &>>${LOG_FILE} && systemctl start mongod   &>>${LOG_FILE}
 ECHO "MongoDB Installed"
+checkRootUser $?                   &>>${LOG_FILE}
 
+ECHO"Configuring IP ADDRESS"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf      &>>${LOG_FILE}
-
-systemctl start mongod            &>>${LOG_FILE}
 
 ECHO "Unzipping Files"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG_FILE}
-cd /tmp
-unzip mongodb.zip   &>>${LOG_FILE}
-cd mongodb-main
+cd /tmp && unzip mongodb.zip   &>>${LOG_FILE} && cd mongodb-main
 ECHO "Unzipping Successful"
 
-mongo < catalogue.js    &>>${LOG_FILE}
-mongo < users.js        &>>${LOG_FILE}
+mongo < catalogue.js  &>>${LOG_FILE}   &&   mongo < users.js        &>>${LOG_FILE}
 
 systemctl restart mongod            &>>${LOG_FILE}
 
