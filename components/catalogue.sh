deddb7d58b6e1258b4be526f48b9295fea33c47f
MH <mrhh94@live.com>
2022-05-08 17:16:28 -0500

    Roboshop Project

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -11,16 +11,16 @@ ECHO "Installing NODE.JS"
 yum install nodejs gcc-c++ -y   &>>${LOG_FILE}
 checkStatus $?
 
-id roboshop #&>>${LOG_FILE}
+id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
   ECHO "Add Application User"
-  useradd roboshop    #&>>${LOG_FILE}
+  useradd roboshop    &>>${LOG_FILE}
   checkStatus $?
   exit 0
 fi
 
 ECHO "Download Application"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"    #&>>${LOG_FILE}
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"    &>>${LOG_FILE}
 
 
 ECHO "Extract Application Archive"
