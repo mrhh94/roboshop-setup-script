425e4e85debf4c367aaa8df9b86e2bbd76a0c7dd
MH <mrhh94@live.com>
2022-05-08 16:47:07 -0500

    Roboshop Project

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
-echo catalogue
+source components/common.sh
+checkRootUser $?                  &>>${LOG_FILE}
 
+ECHO "Installing NODE.JS Repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${LOG_FILE} &&
+checkStatus $?
+
+ECHO "Installing NODE.JS"
+yum install nodejs gcc-c++ -y   &>>${LOG_FILE}
+checkStatus $?
+
+id roboshop &>>${LOG_FILE}
+if [ $1 -eq 0 ]; then
+  ECHO "Add Application User"
+  useradd roboshop    &>>${LOG_FILE}
+  checkStatus $?
+fi
+
+ECHO "Download Application"
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"    &>>${LOG_FILE}
+
+
+ECHO "Extract Application Archive"
+$ cd /home/roboshop
+$ unzip /tmp/catalogue.zip
+$ mv catalogue-main catalogue
+$ cd /home/roboshop/catalogue
+$ npm install
