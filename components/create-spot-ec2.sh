db1d43d08c8033c49b0d8b37ac8d59f97d03c3c1
MH <mrhh94@live.com>
2022-05-07 20:33:27 -0500

    Roboshop Project

--- components/create-spot-ec2.sh
+++ components/create-spot-ec2.sh
@@ -9,7 +9,7 @@ NAME=$1
 
 aws ec2 describe-spot-instance-requests \
     --filters Name=tag:Name,Values="${NAME}" Name=state,Values=active \
-    --output table | grep InstanceId | awk '{print $5}' &>/dev/null
+    --output table | grep InstanceId &>/dev/null
 
 if [ "$1" -eq 0 ]; then
   echo "Instance Already Exists"
