8671470efb6cc2696f8a101655e0fb6ffbb75135
MH <mrhh94@live.com>
2022-05-07 19:49:29 -0500

    Roboshop Project

--- components/create-spot-ec2.sh
+++ components/create-spot-ec2.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+  echo "Instance argument required"
+  exit 1
+fi
+
 NAME=$1
 
+AMI_ID=$(
+  aws ec2 describe-images \
+      --filters "Name=name,Values=Centos-7-DevOps-Practice" \
+      --output table | grep ImageId | awk '{print $4}'
+)
+
 aws ec2 run-instances \
-      --image-id ami-0bb6af715826253bf \
+      --image-id "${AMI_ID}" \
       --instance-type t2.micro \
       --count 1 --subnet-id subnet-550b0a18 \
       --security-group-ids sg-0cfbae2747cbac51a \
