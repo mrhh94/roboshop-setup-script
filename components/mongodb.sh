8ea0d092ced4e372e31628791734a136495dcfeb
MH <mrhh94@live.com>
2022-05-08 09:50:21 -0500

    Roboshop Project

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -6,22 +6,26 @@ checkRootUser $?                    &>>${LOG_FILE}
 
 ECHO "Installing MongoDB"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo    &>>${LOG_FILE}
-checkRootUser $?                   &>>${LOG_FILE}
-
 yum install -y mongodb-org  &>>${LOG_FILE} && systemctl enable mongod  &>>${LOG_FILE} && systemctl start mongod   &>>${LOG_FILE}
-ECHO "MongoDB Installed"
+checkStatus $?
 
 ECHO "Configuring IP ADDRESS"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf      &>>${LOG_FILE}
+checkStatus $?
 
 ECHO "Unzipping Files"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG_FILE}
-cd /tmp && unzip mongodb.zip   &>>${LOG_FILE} && cd mongodb-main
-ECHO "Unzipping Successful" &&
+cd /tmp
+unzip mongodb.zip   &>>${LOG_FILE}
+checkStatus $?
 
-mongo < catalogue.js  &>>${LOG_FILE}   &&   mongo < users.js        &>>${LOG_FILE}
+ECHO "Downloading Schema"
+cd mongodb-main
+mongo < catalogue.js  &>>${LOG_FILE}
+mongo < users.js        &>>${LOG_FILE}
+checkStatus $?
 
 systemctl restart mongod            &>>${LOG_FILE}
-
-ECHO "MongoDB Started Successfully"
+ECHO "MongoDB Starting"
+checkStatus $?
 
