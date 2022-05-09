0095447194fd6ea36a42040ec3a34731008c9dec
MH <mrhh94@live.com>
2022-05-08 20:32:50 -0500

    Roboshop Project

--- components/create-ec2.sh
+++ components/create-ec2.sh
@@ -29,5 +29,5 @@ INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Va
 IPADDRESS=$(aws ec2 describe-instances  --instance-ids ${INSTANCE_ID}  --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
 
 sed -e "s/COMPONENT/${NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z07578712H75FS9NNU2HC --change-batch file:///tmp/record.json &>/dev/null
+aws route53 change-resource-record-sets --hosted-zone-id Z050212011DFLHIMYY41B --change-batch file:///tmp/record.json &>/dev/null
 echo DNS Record Created
